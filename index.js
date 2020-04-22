36e1191e32d4ab862718b13840cb303b3ac1536a
El Sonny <sonny@spaceshiplabs.com>
2020-04-21 21:53:04 -0500

    new source files and finish mapping cases

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-'use strict'
-
+const fs = require('fs').promises;
 const Fs = require('fs');
 const Path = require('path');
 const Axios = require('axios');
@@ -14,9 +13,7 @@ async function download(url) {
         method: 'GET',
         responseType: 'stream'
     });
-
     response.data.pipe(writer)
-
     return new Promise((resolve, reject) => {
         writer.on('finish', resolve)
         writer.on('error', reject)
@@ -25,7 +22,7 @@ async function download(url) {
 
 async function deflate() {
     return new Promise((resolve, reject) => {
-    	const unzipper = new DecompressZip(path)
+        const unzipper = new DecompressZip(path)
         unzipper.on('extract', resolve);
         unzipper.on('error', reject);
         unzipper.extract({ path: Path.resolve(__dirname, 'data', 'source', 'csv') });
@@ -36,14 +33,53 @@ async function parse(file) {
     return csv().fromFile(Path.resolve(__dirname, 'data', 'source', 'csv', file));
 };
 
+function agregate(entry, currentData) {
+    let newData = { ...currentData };
+    newData.total += 1;
+    if (entry.RESULTADO === '1') newData.confirmed += 1;
+    if (entry.RESULTADO === '2') newData.negative += 1;
+    if (entry.RESULTADO === '3') newData.suspicious += 1;
+    if (entry.FECHA_DEF !== '9999-99-99' && entry.FECHA_DEF !== '' && entry.RESULTADO === '1'){
+        newData.deaths += 1;
+    }else if(entry.RESULTADO === '1' ){
+        const start = new Date(entry.FECHA_INGRESO);
+        const fileDate = new Date(entry.FECHA_ACTUALIZACION);
+        const daysDiff = (fileDate - start) / (24 * 60 * 60 * 1000);
+        if(daysDiff >= 14){
+            newData.recoveries += 1; 
+        }else{
+            newData.active += 1;
+        }
+    }
+    return newData;
+};
+
+
+
 async function execute() {
-    console.log('downloading')
     await download('http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip');
-    console.log('deflating');
     const zipLog = await deflate();
-    console.log('parsing');
-    const json = await parse(zipLog[0].deflated);
-    console.log(json.length);
+    const entries = await parse(zipLog[0].deflated);
+    //const entries = await parse('200420COVID19MEXICO.csv');
+    let municipalities = {};
+    entries.forEach(entry => {
+        const compoundKey = entry.ENTIDAD_RES + entry.MUNICIPIO_RES;
+        if (!municipalities[compoundKey]) {
+            municipalities[compoundKey] = {
+                total: 0,
+                suspicious: 0,
+                confirmed: 0,
+                deaths: 0,
+                recoveries: 0,
+                negative: 0,
+                active : 0,
+            }
+        };
+        municipalities[compoundKey] = agregate(entry, municipalities[compoundKey]);
+    });
+
+
+    //await fs.writeFile('./temp/m.json', JSON.stringify(municipalities));
 };
 
 execute();
